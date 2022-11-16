690478f8e052cc081c0a5be13422800f4e969f32
thrila <ifeanyichukwundubuizu@gmail.com>
2022-11-17 00:26:08 +0100

    Build - Style and functionality

---
+++ index.js
@@ -0,0 +1,32 @@
+// https://openlibrary.org/search/authors.json?q=mark%20manson
+const Key = document.getElementById('key');
+const AuthorName = document.getElementById('author-name');
+const TopWork = document.getElementById('top-work');
+const WorkCount = document.getElementById('work-count');
+const Unordered = document.querySelector('.list');
+const Btn = document.getElementById('button');
+let Input = document.getElementById('input');
+
+Btn.addEventListener("click", AuthorNam);
+
+
+
+async function AuthorNam (){
+    const string = Input.value;
+    string.replace(" ","%20");
+    const Url = `https://openlibrary.org/search/authors.json?q=${string}`
+    const response = await fetch(Url);
+    const data = await response.json()
+    const {key,name,top_subjects ,top_work,work_count}=data.docs[0]
+    Key.innerText = key;
+    AuthorName.innerText = name;
+    TopWork.innerText = top_work;
+    WorkCount.innerText= work_count
+    top_subjects.map((subject)=> {
+        console.log(subject);
+        const List = document.createElement('li');
+        List.classList.add("list-el")
+        List.innerText= subject;
+        Unordered.appendChild(List)
+    });
+}
