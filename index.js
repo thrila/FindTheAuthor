760d279d4a7fcf76fd5b1364171612e33d649b29
thrila <ifeanyichukwundubuizu@gmail.com>
2022-11-26 23:41:10 +0100

    Fix - functionality and design

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Key = document.getElementById('key');
 const AuthorName = document.getElementById('author-name');
 const TopWork = document.getElementById('top-work');
 const WorkCount = document.getElementById('work-count');
-const Unordered = document.querySelector('.list');
+let Unordered = document.querySelector('.list');
 const Btn = document.getElementById('button');
 let Input = document.getElementById('input');
 
@@ -17,17 +17,23 @@ async function AuthorNam (){
     string.replace(" ","%20");
     const Url = `https://openlibrary.org/search/authors.json?q=${string}`
     const response = await fetch(Url);
-    const data = await response.json()
-    const {key,name,top_subjects ,top_work,work_count}=data.docs[0]
-    Key.innerText = key;
+    const data = await response.json();
+    const {name,top_subjects ,top_work,work_count}=data.docs[0];
+    let List = ''
     AuthorName.innerText = name;
     TopWork.innerText = top_work;
     WorkCount.innerText= work_count
     top_subjects.map((subject)=> {
         console.log(subject);
-        const List = document.createElement('li');
+         List = document.createElement('li');
         List.classList.add("list-el")
         List.innerText= subject;
         Unordered.appendChild(List)
+        
     });
+    console.log(List)
+    
+    Input.value = ""
+    // Unordered.removeChild(List)
+    
 }
